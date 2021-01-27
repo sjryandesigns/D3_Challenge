77dca67e832caa6655c327ff2f8fe3e71f8c62ba
sjryandesigns <sjryandesigns@gmail.com>
2021-01-27 13:56:04 -0800

    editing axis variables

--- StarterCode/assets/js/bonus.js
+++ StarterCode/assets/js/bonus.js
@@ -86,7 +86,7 @@ function updateToolTips (circleGroup, selectedXAxis, selectedYAxis){
         var xlabel = "In Poverty (%): ";
     }
     else if (selectedXAxis) === "age"){
-        var xlabelk = "Age (Median): ";
+        var xlabel = "Age (Median): ";
     }
     else{
         var xlabel = "Household Income (Median): $";
@@ -122,118 +122,108 @@ function updateToolTips (circleGroup, selectedXAxis, selectedYAxis){
 
 };
 
+// Access data from csv and load in. Create then function
+d3.csv("assets/data/data.csv").then(function(stateData){
+    console.log(stateData);
 
+    stateData.forEach(function(d){
+        d.poverty = +d.poverty;
+        d.age = +d.age;
+        d.income = +d.income;
+        d.healthcare = +d.healthcare;
+        d.smokes = +d.smokes;
+        d.obesity = +d.obesity;
+        d.abbr = d.abbr;
 
+    });
 
-
-
-
-
-
-
-
-
-
-
-
-// // Access data from csv and load in. Create then function
-// d3.csv("assets/data/data.csv").then(function(stateData){
-//     console.log(stateData);
-
-//     stateData.forEach(function(d){
-//         d.poverty = +d.poverty;
-//         d.healthcare = +d.healthcare;
-//         d.abbr = d.abbr;
-//     });
-
-//     // Create band scale for horizontal and vertical axes
-//     var xScale = d3.scaleLinear()
-//         .domain([d3.min(stateData, d=>d.poverty)-1, d3.max(stateData, d => d.poverty)+1])
-//         .range([0, width]);
-
-//     var yScale = d3.scaleLinear()
-//         .domain([d3.min(stateData, d=>d.healthcare)-1, d3.max(stateData, d => d.healthcare)+1])
-//         .range([height, 0]);
-
-//     // Create functions and pass scales as arguments to create chart axes
-//     var bottomAxis = d3.axisBottom(xScale);
-//     var leftAxis = d3.axisLeft(yScale);
-
-//     // Append group elements to chartGroup area and create axes 
-//     chartGroup.append("g")
-//         .call(leftAxis);
-
-//     chartGroup.append("g")
-//         .attr("transform", `translate(0, ${height})`)
-//         .call(bottomAxis);
-
-//     // Append circles to chart
-//     var circlesGroup = chartGroup.selectAll("circle")
-//         .data(stateData)
-//         .enter()
-//         .append("circle")
-//         .attr("cx", d => xScale(d.poverty))
-//         .attr("cy", d => yScale(d.healthcare))
-//         .attr("r", 15)
-//         .classed("stateCircle", true);
-
-//     // Append text to circles on chart
-//     var circlesText = chartGroup.selectAll(".stateText")
-//         .data(stateData)
-//         .enter()
-//         .append("text")
-//         .classed("stateText", true)
-//         .attr("x", d => xScale(d.poverty))
-//         .attr("y", d => yScale(d.healthcare))
-//         .text(d => (d.abbr))
-//         .attr("text-anchor", "middle")
-//         .attr("alignment-baseline", "central");
+    // Create band scale for horizontal and vertical axes
+    var xLinearScale = xScale(stateData, selectedXAxis);
     
-//     // Append text for axes to chart
-//     chartGroup.append("text")
-//         .attr("transform", `translate(${width / 2}, ${height + margin.top})`)
-//         .classed("aText", true)
-//         .classed("active", true)
-//         .text("In Poverty (%)");
+    var yLinearScale = yScale(stateData, selectedYAxis);
+
+
+    // Create functions and pass scales as arguments to create chart axes
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    // Append group elements to chartGroup area and create axes 
+    var xAxis = chartGroup.append("g")
+        .classed("x-axis", true)
+        .attr("transform", `translate(0, ${height})`)
+        .call(bottomAxis);
+
+    var yAxis = chartGroup.append("g")
+        .classed("y-axis", true)
+        .call(leftAxis);
+
+    // Append circles to chart
+    var circlesGroup = chartGroup.selectAll("circle")
+        .data(stateData)
+        .enter()
+        .append("circle")
+        .attr("cx", d => xScale(d.poverty))
+        .attr("cy", d => yScale(d.healthcare))
+        .attr("r", 15)
+        .classed("stateCircle", true);
+
+    // Append text to circles on chart
+    var circlesText = chartGroup.selectAll(".stateText")
+        .data(stateData)
+        .enter()
+        .append("text")
+        .classed("stateText", true)
+        .attr("x", d => xScale(d.poverty))
+        .attr("y", d => yScale(d.healthcare))
+        .text(d => (d.abbr))
+        .attr("text-anchor", "middle")
+        .attr("alignment-baseline", "central");
+    
+    // Append text for axes to chart
+    chartGroup.append("text")
+        .attr("transform", `translate(${width / 2}, ${height + margin.top})`)
+        .classed("aText", true)
+        .classed("active", true)
+        .text("In Poverty (%)");
         
       
-//     chartGroup.append("text")
-//         .attr("transform", "rotate(-90)")
-//         .attr("x", 0-height/2)
-//         .attr("y", 0-margin.left)
-//         .attr("dy", "1em")
-//         .classed("aText", true)
-//         .classed("active", true)
-//         .text("Lacks Healthcare (%)");
+    chartGroup.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("x", 0-height/2)
+        .attr("y", 0-margin.left)
+        .attr("dy", "1em")
+        .classed("aText", true)
+        .classed("active", true)
+        .text("Lacks Healthcare (%)");
 
 
-//     // Add tooltip using d3-tip
-//     var toolTip = d3.tip()
-//         .attr("class", "d3-tip")
-//         .offset([80, -60])
-//         .html(function(d){
+    // Add tooltip using d3-tip
+    var toolTip = d3.tip()
+        .attr("class", "d3-tip")
+        .offset([80, -60])
+        .html(function(d){
             return (`${d.state}, ${d.abbr} <br>Poverty: ${d.poverty}% <br>Healthcare: ${d.healthcare}%`);
         });
-//     chartGroup.call(toolTip);
+    chartGroup.call(toolTip);
 
-//     // Create events for mouse over and mouse out for tool tips
-//     circlesGroup.on("mouseover", function(d){
-//         toolTip.show(d,this);
-//     })
+    // Create events for mouse over and mouse out for tool tips
+    circlesGroup.on("mouseover", function(d){
+        toolTip.show(d,this);
+    })
 
-//         .on("mouseout", function(d){
-//             toolTip.hide(d);
-//         });
+        .on("mouseout", function(d){
+            toolTip.hide(d);
+        });
 
-//     circlesText.on("mouseover", function(d){
-//         toolTip.show(d,this);
-//     })
+    circlesText.on("mouseover", function(d){
+        toolTip.show(d,this);
+    })
     
-//         .on("mouseout", function(d){
-//             toolTip.hide(d);
-//         });
-// }
-// // Error function
-// , function(error) {
-//   console.log(error);
-// });
+        .on("mouseout", function(d){
+            toolTip.hide(d);
+        });
+}
+// Error function
+, function(error) {
+  console.log(error);
+});
