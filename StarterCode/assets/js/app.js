b877f2c8f3009ab569c2d387e05da380bdb3a737
sjryandesigns <sjryandesigns@gmail.com>
2021-01-27 10:07:01 -0800

    SVG Area

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,1 +1,18 @@
 // @TODO: YOUR CODE HERE!
+
+// Define SVG area
+var svgArea = d3.select("body").select("svg");
+
+var svgWidth = window.innerWidth;
+var svgHeight = window.innerHeight;
+
+var margin = {
+  top: 50,
+  right: 50,
+  bottom: 50,
+  left: 50
+};
+
+var height = svgHeight - margin.top - margin.bottom;
+var width = svgWidth - margin.left - margin.right;
+
